f6de4bc56b6349d64aa2346d9a59671ab677d52e
noman-swe <noman.swe130@gmail.com>
2022-02-18 23:56:55 +0600

    30_5-3--> only integer value will pass through the input field

--- js/functional.js
+++ js/functional.js
@@ -1,14 +1,14 @@
 // input function
-function getInputValue(inputId){
+function getInputValue(inputId) {
     const inputField = document.getElementById(inputId);
     const inputFieldValue = parseFloat(inputField.value);
     inputField.value = '';
     return inputFieldValue;
-    
+
 }
 
 // update Total in box
-function getUpdateAmount(inputId, amount){
+function getUpdateAmount(inputId, amount) {
     const totalTag = document.getElementById(inputId);
     const previousAmountTxt = totalTag.innerText;
     const previousAmount = parseFloat(previousAmountTxt);
@@ -19,29 +19,52 @@ function getUpdateAmount(inputId, amount){
 }
 
 // update balance
-function getUpdateBalance(amount, isAdding){
+function getUpdateBalance(amount, isAdding) {
     const balanceTag = document.getElementById('balance-total');
-    console.log(balanceTag);
     const balanceInText = balanceTag.innerText;
     const previousBalance = parseFloat(balanceInText);
     let newBalanceTotal;
-    if(isAdding == true){
+    if (isAdding == true) {
         newBalanceTotal = previousBalance + amount;
-    }else{
+    } else {
         newBalanceTotal = previousBalance - amount;
     }
     balanceTag.innerText = newBalanceTotal;
 }
 
-document.getElementById('deposit-btn').addEventListener('click', function(){
+// error msg
+function getErrorMsg(errorId, updateAmount) {
+    const errorMsg = document.getElementById(errorId);
+    errorMsg.style.color = 'red';
+    if(isNaN(updateAmount) != false ){
+        const errorMsgShow = errorMsg.style.display = 'block';
+        console.log(errorMsgShow + "xxxx");
+    }
+    else if( isNaN(updateAmount) != true){
+        // not working
+        console.log(updateAmount);
+        const errorMsgBlock = errorMsg.style.display = 'none'; 
+        console.log( 'aaaa');
+    }
+}
+
+document.getElementById('deposit-btn').addEventListener('click', function () {
     const depositInputValue = getInputValue('deposit-input');
-    const depositUpdateAmount = getUpdateAmount('deposit-total', depositInputValue);
-    const updateBalanceTotal = getUpdateBalance(depositInputValue, true);
+    if (depositInputValue > 0) {
+        const depositUpdateAmount = getUpdateAmount('deposit-total', depositInputValue);
+        const updateBalanceTotal = getUpdateBalance(depositInputValue, true);
+    } else {
+        getErrorMsg('deposit-error', depositInputValue);
+        
+    }
 });
-document.getElementById('withdraw-btn').addEventListener('click', function(){
+document.getElementById('withdraw-btn').addEventListener('click', function () {
     const withdrawInputValue = getInputValue('withdraw-input');
-    const withdrawUpdateAmount = getUpdateAmount('withdraw-total', withdrawInputValue);
-    const updateBalanceTotal = getUpdateBalance(withdrawInputValue, false);
-    // console.log(withdrawInputValue);
+    if (withdrawInputValue > 0) {
+        const withdrawUpdateAmount = getUpdateAmount('withdraw-total', withdrawInputValue);
+        const updateBalanceTotal = getUpdateBalance(withdrawInputValue, false);
+    } else {
+        getErrorMsg('withdraw-error', withdrawInputValue);
+    }
 });
 
