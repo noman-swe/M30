e9e51db656cf2e3309a2fdf732598c7f7291be0e
noman-swe <noman.swe130@gmail.com>
2022-02-18 17:14:06 +0600

    30_5-2 Share function for deposit and withdraw and balance

--- js/functional.js
+++ js/functional.js
@@ -14,15 +14,34 @@ function getUpdateAmount(inputId, amount){
     const previousAmount = parseFloat(previousAmountTxt);
     const newTotal = previousAmount + amount;
     totalTag.innerText = newTotal;
+    return newTotal;
+    // console.log('newTotal: ' + newTotal);
 }
 
+// update balance
+function getUpdateBalance(amount, isAdding){
+    const balanceTag = document.getElementById('balance-total');
+    console.log(balanceTag);
+    const balanceInText = balanceTag.innerText;
+    const previousBalance = parseFloat(balanceInText);
+    let newBalanceTotal;
+    if(isAdding == true){
+        newBalanceTotal = previousBalance + amount;
+    }else{
+        newBalanceTotal = previousBalance - amount;
+    }
+    balanceTag.innerText = newBalanceTotal;
+}
 
 document.getElementById('deposit-btn').addEventListener('click', function(){
     const depositInputValue = getInputValue('deposit-input');
-    const updateAmount = getUpdateAmount('deposit-total', depositInputValue);
+    const depositUpdateAmount = getUpdateAmount('deposit-total', depositInputValue);
+    const updateBalanceTotal = getUpdateBalance(depositInputValue, true);
 });
 document.getElementById('withdraw-btn').addEventListener('click', function(){
     const withdrawInputValue = getInputValue('withdraw-input');
+    const withdrawUpdateAmount = getUpdateAmount('withdraw-total', withdrawInputValue);
+    const updateBalanceTotal = getUpdateBalance(withdrawInputValue, false);
     // console.log(withdrawInputValue);
 });
 
